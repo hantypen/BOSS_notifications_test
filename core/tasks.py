5f37dc1cdc551022bd359ab1787978671110c9bc
Eric-Melb <eric.mckibbin@gmail.com>
2020-03-12 19:22:36 +0200

    more elaborate callback response

--- core/tasks.py
+++ core/tasks.py
@@ -11,18 +11,21 @@ def tasks_endpoint(event, context):
 
     task_json = json.dumps(tasks)
 
-    return task_json
+    return task_json, 200, {'Content-Type': 'application/json'}
 
 
 @kepler_app.triggered_task(route="/workshop/callback", method="POST")
 def callback_endpoint(event, context):
     logging.info("Responding to callback", extra={"event": event})
 
-    response = {
-        "statusCode": 200,
-        "headers": {"content-type": "application/json"},
-        "body": "OK",
-        "message": "test_message"
-    }
+    data = event.get('json', {}).get('body')
 
-    return json.dumps(response)
+    response = {"message": "No Response Selected"}
+
+    if data.get('action'):
+        if data['action'] == 'ok':
+            response = {"message": "Okay then"}
+        elif data['action'] == 'no':
+            response = {"message": "Sorry :("}
+
+    return json.dumps(response), 200, {'Content-Type': 'application/json'}
