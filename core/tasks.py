8e769ff679bea2420bccd19e7bf396769bd6f9e4
Eric-Melb <eric.mckibbin@gmail.com>
2020-03-12 18:59:46 +0200

    callback endpoint

--- core/tasks.py
+++ core/tasks.py
@@ -12,3 +12,17 @@ def tasks_endpoint(event, context):
     task_json = json.dumps(tasks)
 
     return task_json
+
+
+@kepler_app.triggered_task(route="/workshop/callback", method="POST")
+def callback_endpoint(event, context):
+    logging.info("Responding to callback", extra={"event": event})
+
+    response = {
+        "statusCode": 200,
+        "headers": {"content-type": "application/json"},
+        "body": "OK",
+        "message": "test_message"
+    }
+
+    return response
