a4a06ba3ec2c666556cc3794cbf98a6b4ae78223
Eric-Melb <eric.mckibbin@gmail.com>
2020-03-13 10:24:01 +0200

    Pull out payload data and log issues

--- core/tasks.py
+++ core/tasks.py
@@ -18,11 +18,12 @@ def tasks_endpoint(event, context):
 def callback_endpoint(event, context):
     logging.info("Responding to callback", extra={"event": event})
 
-    data = event.get('data')
+    body = event.get('json', {}).get('body')
 
     try:
-        action = data['actions'][0]['name']
-    except KeyError:
+        action = body['data']['actions'][0]['name']
+    except (KeyError, TypeError) as e:
+        logging.error(f"Got error {str(e)} when trying to get action name", extra={"body": body})
         response = {"message": "Sorry, still working on this :/"}
         return json.dumps(response), 200, {'Content-Type': 'application/json'}
 
